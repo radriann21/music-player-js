48de6c147fabdc97d59254fdddf9318148427ac4
radriann21 <adrianrangelargu@gmail.com>
2024-11-17 21:22:22 -0400

    added dynamic button and corrected list

--- main.js
+++ main.js
@@ -12,6 +12,12 @@ const totalTracks = document.getElementById('total')
 const previousTrack = document.getElementById('back')
 const playPause = document.getElementById('playPause')
 const nextTrack = document.getElementById('forward')
+const svgPlay = ` <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"
+              class="icon icon-tabler icons-tabler-filled icon-tabler-player-play">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" />
+            </svg>`
+const svgPause = `<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-player-pause"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" /><path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" /></svg>`
 
 // audio
 const audio = new Audio()
@@ -28,15 +34,38 @@ function init() {
   trackAlbumName.innerText = data[actual].track_album
 
   audio.src = data[actual].track_direction
+  playPause.innerHTML = svgPlay
 }
 init()
 
 playPause.addEventListener('click', () => {
+  playPause.innerHTML = ''
   if (pause) {
     audio.play()
     pause = false
+    playPause.innerHTML = svgPause
   } else {
     audio.pause()
     pause = true
+    playPause.innerHTML = svgPlay
   }
+})
+
+nextTrack.addEventListener('click', () => {
+  if (actual >= data.length - 1) {
+    actual = 0
+    init()
+  } else {
+    actual++
+    init()
+  }
+})
+
+previousTrack.addEventListener('click', () => {
+  if (actual > 0) {
+    actual--
+  } else {
+    actual = data.length - 1
+  }
+  init()
 })
